57b464b7328c24990d3029ccf692c372fc092edb
Adrian Miasik - GBC 603 <adrianmiasik@gmail.com>
2019-01-26 01:47:44 -0500

    Create a class called main which will control the main game loop

---
+++ Assets/Main.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public CustomerManager customerManager;
+    public SpawnpointManager spawnManager;
+
+    private bool isInitialized = false;
+    
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Update()
+    {
+        if (isInitialized)
+        {
+            if (Developer.allowDebugControls)
+            {
+                // A for Add
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    customerManager.CreateCustomer(spawnManager.GetOpenSpawnpoint());
+                }
+                // R for Remove
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    // If we actually have customers to delete...
+                    if (customerManager.GetAllCustomers().Count >= 1)
+                    {
+                        customerManager.RemoveCustomer(customerManager.GetRandomCustomer());
+                    }
+                }
+            }
+        }
+    }
+
+    public void Initialize()
+    {
+        spawnManager.Generate();
+        isInitialized = true;
+    }
+}
