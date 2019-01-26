efe0a57af61027fd764e16cd7320b48f2d14569e
HyunJung <hjung1017@gmail.com>
2019-01-25 22:40:16 -0500

    customer gets angry

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -1,8 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CrabCustomer : MonoBehaviour
+public class Customer : MonoBehaviour
 {
+    private float m_tolerance = 10f;
+    private float m_timer;
+    private Image m_customerImage;
+
+    [SerializeField] private Sprite m_angryFace;
+
+    private void Update()
+    {
+        m_timer += Time.deltaTime;
+        if (m_timer >= m_tolerance * 0.5f)
+        {
+            GetAngry();
+        }
+
+        if (m_timer >= m_tolerance)
+        {
+            WalkAway();
+        }
+    }
+
+    private void GetAngry()
+    {
+        m_customerImage.sprite = m_angryFace;
+    }
+
+    private void WalkAway()
+    {
+        //TODO: Lose Score
+        //TODO: Clear customer slot
+        //TODO: Show Walkaway animation
+    }
 }
     
