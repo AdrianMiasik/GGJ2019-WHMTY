5b865d7cdd1fcd797851aebde47b5d167729ed60
HyunJung <hjung1017@gmail.com>
2019-01-25 23:01:30 -0500

    add score manager

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreManager : Singleton<ScoreManager>
+{
+    [SerializeField] private TextMeshProUGUI m_scoreText;
+
+    private int m_score;
+
+    public void AddScore(int Score)
+    {
+        m_score += Score;
+        m_scoreText.text = m_score.ToString(); 
+    }
+}
