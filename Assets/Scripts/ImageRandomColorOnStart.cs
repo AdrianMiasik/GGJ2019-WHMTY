817d03dbd6f8d8d8edbc6f366415c79618a70927
Adrian Miasik - GBC 603 <adrianmiasik@gmail.com>
2019-01-25 22:23:25 -0500

    Customer Core + UI Color Tool

---
+++ Assets/Scripts/ImageRandomColorOnStart.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ImageRandomColorOnStart : MonoBehaviour
+{
+    public Image image;
+
+    private void Reset()
+    {
+        if (image == null)
+        {
+            // Attempt to get an image
+            image = GetComponent<Image>();
+        }
+
+        if (image == null)
+        {
+            Debug.LogAssertion("Could not find a image component.");
+        }
+    }
+
+    public void GenerateRandomColor()
+    {
+        image.color = Random.ColorHSV();
+    }
+}
---
+++ Assets/Scripts/UI/ImageRandomColorOnStartButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ImageRandomColorOnStart))]
+public class ImageRandomColorOnStartButton : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ImageRandomColorOnStart script = (ImageRandomColorOnStart) target;
+        if (GUILayout.Button("Generate Random Color"))
+        {
+            script.GenerateRandomColor();
+        }
+    }
+}
