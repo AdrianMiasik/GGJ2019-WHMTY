63a209d17b077dfa2906cfa859abbe7f5bdf43c6
HyunJung <hjung1017@gmail.com>
2019-01-26 16:22:08 -0500

    add game manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject shellPrefab;
+    public Transform shellSpawner;
+
+    public TMPro.TextMeshProUGUI scoreText;
+    public int score;
+
+    public Transform lifeIcons;
+    public int life = 5;
+
+    private void Start()
     {
-        
+        CreateNewShell();
+        UpdateScore();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateNewShell()
     {
-        
+        Instantiate(shellPrefab, shellSpawner);
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = string.Format("Score: {0}", score);
+    }
+
+    private void UpdateLife()
+    {
+        for (int i = 0; i < lifeIcons.childCount; i++)
+        {
+            if (i <= life - 1)
+            {
+                lifeIcons.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                lifeIcons.GetChild(i).gameObject.SetActive(false);
+            }
+        }
     }
 }
