0a17aa6c01d59e0bdac54cfa4184bf599d1f8d4e
HyunJung <hjung1017@gmail.com>
2019-01-27 01:16:29 -0500

    Add main menu scene

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    public TextMeshProUGUI title;
+    public GameObject buttons;
+
+    private void Start()
+    {
+        buttons.SetActive(false);
+        title.color = new Color(title.color.a, title.color.g, title.color.b, 0);
+        float alpha = new float();
+        StartCoroutine(ShowTitle(alpha));
+    }
+
+    private IEnumerator ShowTitle(float alpha)
+    {
+        while (title.color.a < 1)
+        {
+            alpha += 0.5f * Time.deltaTime;
+            title.color = new Color(title.color.r, title.color.g, title.color.b, alpha);
+            yield return null;
+        }
+
+        title.color = new Color(title.color.a, title.color.g, title.color.b, 255);
+        ShowButtons();
+    }
+
+    private void ShowButtons()
+    {
+        buttons.SetActive(true);
+    }
+
+    public void OnPlayButtonPress()
+    {
+        SceneManager.LoadScene("CustomerDemo");
+    }
+
+    public void OnSettingsButtonPress()
+    {
+
+    }
+
+    public void OnCreditButtonPress()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
